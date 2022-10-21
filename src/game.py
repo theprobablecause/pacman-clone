6a97d6a48013e6e457a92881af2d054085a2b47d
NaeemK1188 <naeemalbirkdar@aol.com>
2022-10-20 23:31:50 -0700

    menu in main

--- src/game.py
+++ src/game.py
@@ -1,12 +1,12 @@
 import os
 import pygame as pg
-
 import play
+from menu import Menu
 
 class Game:
     RESOLUTION = (1280, 800)
-    FRAMES_PER_SECOND = 60
-    FRAME_TIME = 1.0/FRAMES_PER_SECOND
+    FRAMES_PER_SECOND = 60 # limit the game speed 
+    FRAME_TIME = 1.0/FRAMES_PER_SECOND 
     PROJECT_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
 
     def __init__(self):
@@ -15,13 +15,16 @@ class Game:
         self.frame_clock = pg.time.Clock()
 
     def run(self):
-        while True:
-            # TODO: replace with main menu
-            p = play.Play(game=self)
-            p.run()
+        while True: 
+            g = Menu()
+            g.main_menu()
+
+
+
+            
     
     # Draw current display buffer to screen, then wait for next frame.
     # NOTE: Call at the end of a frame process (end of the running loop)
     def wait_next_frame(self):
-        pg.display.flip()
+        pg.display.flip() # update the screen 
         self.frame_clock.tick(Game.FRAMES_PER_SECOND) # wait until next frame time
